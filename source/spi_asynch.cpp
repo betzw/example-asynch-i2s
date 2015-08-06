f54d8d16bfc2f5db0ee8c76dbb559d041713b296
Martin Kojtal <c0170@rocketmail.com>
2015-08-06 09:40:05 +0100

    The example is a simple Test class with prints, using scheduler

--- source/spi_asynch.cpp
+++ source/spi_asynch.cpp
@@ -1,5 +1,6 @@
+
 /* mbed Microcontroller Library
- * Copyright (c) 2015 ARM Limited
+ * Copyright (c) 2013 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,16 +14,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include <TestHarness.h>
-#include <mbed.h>
-#include <SPI.h>
-#include <spi_api.h>
+#include "mbed.h"
 #include <stdio.h>
+#include "minar/minar.h"
+#include "Event.h"
 
-#if !DEVICE_SPI
-#error spi_master_asynch requires SPI
-#endif
-
+#if DEVICE_SPI_ASYNCH
 
 #define SHORT_XFR 3
 #define LONG_XFR 16
@@ -45,266 +42,71 @@
 #error Target not supported
 #endif
 
-volatile int why;
-volatile bool complete;
-void cbdone(int event) {
-    complete = true;
-    why = event;
-}
+using namespace minar;
 
+class SPITest {
 
-TEST_GROUP(SPI_Master_Asynchronous)
-{
-    uint8_t tx_buf[LONG_XFR];
-    uint8_t rx_buf[LONG_XFR];
-    SPI *obj;
-    DigitalOut *cs;
-    event_callback_t callback;
-
-    void setup() {
-        obj = new SPI(TEST_MOSI_PIN, TEST_MISO_PIN, TEST_SCLK_PIN);
-        cs = new DigitalOut(TEST_CS_PIN);
-        complete = false;
-        why = 0;
-        callback.attach(cbdone);
-
-        // Set the default value of tx_buf
+public:
+    SPITest(): spi(TEST_MOSI_PIN, TEST_MISO_PIN, TEST_SCLK_PIN), cs(TEST_CS_PIN) {
         for (uint32_t i = 0; i < sizeof(tx_buf); i++) {
             tx_buf[i] = i + TEST_BYTE_TX_BASE;
         }
-        memset(rx_buf,TEST_BYTE_RX,sizeof(rx_buf));
+        cs = 1;
     }
-    void teardown() {
-        delete obj;
-        obj = NULL;
-        delete cs;
-        cs = NULL;
+
+    void start() {
+        printf("Starting short transfer test\r\n");
+        init_rx_buffer();
+        cs = 0;
+        printf("Res is %d\r\n", spi.transfer(tx_buf, SHORT_XFR, rx_buf, SHORT_XFR, event_callback_t(this, &SPITest::short_transfer_complete_cb), SPI_EVENT_COMPLETE));
     }
-    uint32_t cmpnbuf(uint8_t *expect, uint8_t *actual, uint32_t offset, uint32_t end, const char *file, uint32_t line)
-    {
-        uint32_t i;
-        for (i = offset; i < end; i++){
-            if (expect[i] != actual[i]) {
-                break;
+
+private:
+    void init_rx_buffer() {
+        for (uint32_t i = 0; i < sizeof(rx_buf); i ++) {
+            rx_buf[i] = 0;
+        }
+    }
+
+    void compare_buffers(uint32_t len) {
+         for (uint32_t i = 0; i < len; i ++) {
+            if (tx_buf[i] != rx_buf[i]) {
+                printf("MISMATCH at position %u: expected %d, got %d\r\n", i, (int)tx_buf[i], (int)rx_buf[i]);
             }
         }
-        if (i < end) {
-            CHECK_EQUAL_LOCATION((int)expect[i], (int)actual[i], file, line);
-        }
-        CHECK_EQUAL_LOCATION(end, i, file, line);
-        return i;
     }
-    uint32_t cmpnbufc(uint8_t expect, uint8_t *actual, uint32_t offset, uint32_t end, const char *file, uint32_t line)
-    {
-        uint32_t i;
-        for (i = offset; i < end; i++){
-            if (expect != actual[i]) {
-                break;
-            }
-        }
-        if (i < end) {
-            CHECK_EQUAL_LOCATION((int)expect, (int)actual[i], file, line);
-        }
-        CHECK_EQUAL_LOCATION(end, i, file, line);
-        return i;
+
+    void short_transfer_complete_cb(int narg) {
+        cs = 1;
+        printf("Short transfer DONE, event is %d\r\n", narg);
+        compare_buffers(SHORT_XFR);
+        printf("Starting long transfer test\r\n");
+        init_rx_buffer();
+        cs = 0;
+        printf("Res is %d\r\n", spi.transfer(tx_buf, LONG_XFR, rx_buf, LONG_XFR, event_callback_t(this, &SPITest::long_transfer_complete_cb), SPI_EVENT_COMPLETE));
     }
-    void dumpRXbuf() {
-        uint32_t i;
-        printf("\r\n");
-        printf("RX Buffer Contents: [");
-        //flushf(stdout);
-        for (i = 0; i < sizeof(rx_buf); i++){
-            printf("%02x",rx_buf[i]);
-            if (i+1 < sizeof(rx_buf)){
-                printf(",");
-            }
-        }
-        printf("]\r\n");
+
+    void long_transfer_complete_cb(int narg) {
+        cs = 1;
+        printf("Long transfer DONE, event is %d\r\n", narg);
+        compare_buffers(LONG_XFR);
+        printf("**** Test done ****\r\n");
     }
+
+private:
+    SPI spi;
+    DigitalOut cs;
+    uint8_t tx_buf[LONG_XFR];
+    uint8_t rx_buf[LONG_XFR];
 };
 
-// SPI write tx length: FIFO-1, read length: 0
-//   Checks: Null pointer exceptions, completion event
-TEST(SPI_Master_Asynchronous, short_tx_0_rx)
-{
-    int rc;
-    // Write a buffer of Short Transfer length.
-    rc = obj->transfer( tx_buf,SHORT_XFR,NULL,0, callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(why, SPI_EVENT_COMPLETE);
-
-    // TODO: Check for a null pointer exception
+void app_start(int, char*[]) {
+    static SPITest test;
+    Scheduler::postCallback(FunctionPointer0<void>(&test, &SPITest::start).bind());
 }
 
-
-//
-// SPI write tx length: FIFO-1, read length: 0, non-null read pointer
-//   Checks: Null pointer exceptions, completion event, canary values in read buffer
-TEST(SPI_Master_Asynchronous, short_tx_0_rx_nn)
-{
-    int rc;
-    // Write a buffer of Short Transfer length.
-    rc = obj->transfer( tx_buf,SHORT_XFR,rx_buf,0,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    // Check that the rx buffer is untouched
-    cmpnbufc(TEST_BYTE_RX,rx_buf,0,sizeof(rx_buf),__FILE__,__LINE__);
+#else
+void app_start(int, char*[]) {
 }
+#endif
 
-// SPI write tx length: 0, read length: FIFO-1
-//   Checks: Receive value==fill character, completion event
-TEST(SPI_Master_Asynchronous, 0_tx_short_rx)
-{
-    int rc;
-    // Read a buffer of Short Transfer length.
-    rc = obj->transfer( NULL,0,rx_buf,SHORT_XFR,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    // TODO: Check for null pointer exception
-    // Check that the receive buffer contains the fill byte.
-    cmpnbufc((uint8_t)SPI_FILL_WORD,rx_buf,0,SHORT_XFR,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,SHORT_XFR,sizeof(rx_buf),__FILE__,__LINE__);
-}
-
-// SPI write tx length: 0, read length: FIFO-1
-//   Checks: Receive value==fill character, completion event
-TEST(SPI_Master_Asynchronous, 0_tx_nn_short_rx)
-{
-    int rc;
-    // Read a buffer of Short Transfer length.
-    rc = obj->transfer(tx_buf,0,rx_buf,SHORT_XFR,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    // Check that the receive buffer contains the fill byte.
-    cmpnbufc((uint8_t)SPI_FILL_WORD,rx_buf,0,SHORT_XFR,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,SHORT_XFR,sizeof(rx_buf),__FILE__,__LINE__);
-}
-
-// SPI write tx length: FIFO-1 ascending values, read length: FIFO-1
-//   Checks: Receive buffer == tx buffer, completion event
-TEST(SPI_Master_Asynchronous, short_tx_short_rx)
-{
-    int rc;
-    // Write/Read a buffer of Long Transfer length.
-    rc = obj->transfer( tx_buf,SHORT_XFR,rx_buf,SHORT_XFR,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    // Check that the rx buffer contains the tx bytes
-    cmpnbuf(tx_buf,rx_buf,0,SHORT_XFR,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,SHORT_XFR,sizeof(rx_buf),__FILE__,__LINE__);
-}
-// SPI write tx length: 2xFIFO ascending values, read length: 2xFIFO
-//   Checks: Receive buffer == tx buffer, completion event
-TEST(SPI_Master_Asynchronous, long_tx_long_rx)
-{
-    int rc;
-    // Write/Read a buffer of Long Transfer length.
-    rc = obj->transfer(tx_buf,LONG_XFR,rx_buf,LONG_XFR,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    //dumpRXbuf();
-    // Check that the rx buffer contains the tx bytes
-    cmpnbuf(tx_buf,rx_buf,0,LONG_XFR,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,LONG_XFR,sizeof(rx_buf),__FILE__,__LINE__);
-}
-
-// SPI write tx length: 2xFIFO, ascending, read length: FIFO-1
-//   Checks: Receive buffer == tx buffer, completion event, read buffer overflow
-TEST(SPI_Master_Asynchronous, long_tx_short_rx)
-{
-    int rc;
-    // Write a buffer of Short Transfer length.
-    rc = obj->transfer(tx_buf,LONG_XFR,rx_buf,SHORT_XFR,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    // Check that the rx buffer contains the tx bytes
-    cmpnbuf(tx_buf,rx_buf,0,SHORT_XFR,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,SHORT_XFR,sizeof(rx_buf),__FILE__,__LINE__);
-}
-
-// SPI write tx length: FIFO-1, ascending, read length: 2xFIFO
-//   Checks: Receive buffer == tx buffer, then fill, completion event
-TEST(SPI_Master_Asynchronous, short_tx_long_rx)
-{
-    int rc;
-    // Write a buffer of Short Transfer length.
-    rc = obj->transfer(tx_buf,SHORT_XFR,rx_buf,LONG_XFR,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    //dumpRXbuf();
-    // Check that the rx buffer contains the tx bytes
-    cmpnbuf(tx_buf,rx_buf,0,SHORT_XFR,__FILE__,__LINE__);
-    // Check that the rx buffer contains the tx fill bytes
-    cmpnbufc((uint8_t)SPI_FILL_WORD,rx_buf,SHORT_XFR,LONG_XFR,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,LONG_XFR,sizeof(rx_buf),__FILE__,__LINE__);
-}
-
-TEST(SPI_Master_Asynchronous, queue_test)
-{
-    int rc;
-    // Write/Read a buffer of Long Transfer length.
-    rc = obj->transfer( tx_buf,4,rx_buf,4,callback, 0);
-    CHECK_EQUAL(0, rc);
-    rc = obj->transfer( &tx_buf[4],4, &rx_buf[4],4,callback, 0);
-    CHECK_EQUAL(0, rc);
-    rc = obj->transfer( &tx_buf[8],4, &rx_buf[8],4,callback, -1);
-    CHECK_EQUAL(0, rc);
-
-    while (!complete);
-
-    // Make sure that the callback fires.
-    CHECK_EQUAL(SPI_EVENT_COMPLETE, why);
-
-    // Check that the rx buffer contains the tx bytes
-    cmpnbuf(tx_buf,rx_buf,0,12,__FILE__,__LINE__);
-    // Check that remaining portion of the receive buffer contains the rx test byte
-    cmpnbufc(TEST_BYTE_RX,rx_buf,12,sizeof(rx_buf),__FILE__,__LINE__);
-}
-
-// On DMA-enabled platforms, add an additional test with large transfers and DMA.
-// To validate DMA, disable non-completion IRQs after starting the transfer.
