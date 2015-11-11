ef6b9516686dcc31f4583c84c84da211b6075c49
Martin Kojtal <martin.kojtal@arm.com>
2015-11-11 14:06:38 +0100

    Source - mbed header comes from mbed-drivers fix

--- source/spi_asynch.cpp
+++ source/spi_asynch.cpp
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "mbed.h"
+#include "mbed-drivers/mbed.h"
 #include <stdio.h>
 #include "minar/minar.h"
 #include "core-util/Event.h"
