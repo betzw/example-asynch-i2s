de9ae43cf6e7eaa3d16447e0f333583b65f3cafb
Wolfgang Betz <wolfgang.betz@st.com>
2015-12-21 13:45:45 +0100

    Remove chip select and move to I2S interface

--- source/i2s_asynch.cpp
+++ source/i2s_asynch.cpp
@@ -1,4 +1,3 @@
-
 /* mbed Microcontroller Library
  * Copyright (c) 2015 ARM Limited
  *
@@ -34,10 +33,12 @@
 #define TEST_BYTE_RX TEST_BYTE3
 #define TEST_BYTE_TX_BASE TEST_BYTE5
 
-#if !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_DPIN) || !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_SCLK) || \
-    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL) || !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX)
-#error This example requires I2S test pins to be defined. Please define the hardware.test-pins.i2s.dpin/ \
-    sclk/wsel/fdpx yotta confing values
+#if !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_DPIN) ||	\
+    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_SCLK) ||	\
+    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL) ||	\
+    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX)
+
+#error This example requires I2S test pins to be defined. Please define the hardware.test-pins.i2s.dpin/sclk/wsel/fdpx yotta confing values
 #endif
 
 using namespace minar;
@@ -46,22 +47,21 @@ class I2STest {
 
 public:
     I2STest(): i2s(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_DPIN, YOTTA_CFG_HARDWARE_TEST_PINS_I2S_SCLK,
-        YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL), cs(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX) {
-        for (uint32_t i = 0; i < sizeof(tx_buf); i++) {
-            tx_buf[i] = i + TEST_BYTE_TX_BASE;
-        }
-        cs = 1;
+		   YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL, YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX) {
+	for (uint32_t i = 0; i < sizeof(tx_buf); i++) {
+	    tx_buf[i] = i + TEST_BYTE_TX_BASE;
+	}
     }
-
+    
     void start() {
         printf("Starting short transfer test\r\n");
         init_rx_buffer();
-        cs = 0;
+
         printf("Res is %d\r\n", i2s.transfer()
-            .tx(tx_buf, SHORT_XFR)
-            .rx(rx_buf, SHORT_XFR)
-            .callback(I2S::event_callback_t(this, &I2STest::short_transfer_complete_cb), I2S_EVENT_RX_COMPLETE)
-            .apply());
+	       .tx(tx_buf, SHORT_XFR)
+	       .rx(rx_buf, SHORT_XFR)
+	       .callback(I2S::event_callback_t(this, &I2STest::short_transfer_complete_cb), I2S_EVENT_RX_COMPLETE)
+	       .apply());
     }
 
 private:
@@ -72,7 +72,7 @@ private:
     }
 
     void compare_buffers(uint32_t len) {
-         for (uint32_t i = 0; i < len; i ++) {
+	for (uint32_t i = 0; i < len; i ++) {
             if (tx_buf[i] != rx_buf[i]) {
                 printf("MISMATCH at position %lu: expected %d, got %d\r\n", i, (int)tx_buf[i], (int)rx_buf[i]);
             }
@@ -82,23 +82,23 @@ private:
     void short_transfer_complete_cb(Buffer tx_buffer, Buffer rx_buffer, int narg) {
         (void)tx_buffer;
         (void)rx_buffer;
-        cs = 1;
+
         printf("Short transfer DONE, event is %d\r\n", narg);
         compare_buffers(SHORT_XFR);
         printf("Starting long transfer test\r\n");
         init_rx_buffer();
-        cs = 0;
+
         printf("Res is %d\r\n", i2s.transfer()
-            .tx(tx_buf, LONG_XFR)
-            .rx(rx_buf, LONG_XFR)
-            .callback(I2S::event_callback_t(this, &I2STest::long_transfer_complete_cb), I2S_EVENT_RX_COMPLETE)
-            .apply());
+	       .tx(tx_buf, LONG_XFR)
+	       .rx(rx_buf, LONG_XFR)
+	       .callback(I2S::event_callback_t(this, &I2STest::long_transfer_complete_cb), I2S_EVENT_RX_COMPLETE)
+	       .apply());
     }
 
     void long_transfer_complete_cb(Buffer tx_buffer, Buffer rx_buffer, int narg) {
         (void)tx_buffer;
         (void)rx_buffer;
-        cs = 1;
+
         printf("Long transfer DONE, event is %d\r\n", narg);
         compare_buffers(LONG_XFR);
         printf("**** Test done ****\r\n");
@@ -106,7 +106,6 @@ private:
 
 private:
     I2S i2s;
-    DigitalOut cs;
     uint8_t tx_buf[LONG_XFR];
     uint8_t rx_buf[LONG_XFR];
 };
@@ -118,6 +117,6 @@ void app_start(int, char*[]) {
 
 #else
 void app_start(int, char*[]) {
-    printf("The target does not support I2S asynch API.\r\n");
+    printf("The target does not support I2S (asynch) API.\r\n");
 }
 #endif
