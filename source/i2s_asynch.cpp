8c0313f2acf0f5c2b60000eccdcfe4ab8b1b0145
Wolfgang Betz <wolfgang.betz@st.com>
2016-01-12 14:48:41 +0100

    Hearing some first noise

--- source/i2s_asynch.cpp
+++ source/i2s_asynch.cpp
@@ -19,6 +19,8 @@
 #include "minar/minar.h"
 #include "core-util/Event.h"
 
+#include "sta350bw/sta350bw.h"
+
 #if DEVICE_I2S
 
 #define XFER_SIZE  (2 * 8192)
@@ -35,9 +37,11 @@
 #if !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_DPIN) ||	\
     !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_SCLK) ||	\
     !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL) ||	\
-    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX)
-
-#error This example requires I2S test pins to be defined. Please define the hardware.test-pins.i2s.dpin/sclk/wsel/fdpx yotta confing values
+    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX) ||  \
+    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_MCLK) ||  \
+	!defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2C_SDA)  ||	\
+    !defined(YOTTA_CFG_HARDWARE_TEST_PINS_I2C_SCL)
+#error This example requires I2S & I2C test pins to be defined. Please define the hardware.test-pins.i2s.dpin/sclk/wsel/fdpx yotta confing values
 #endif
 
 #ifndef NDEBUG
@@ -51,21 +55,42 @@ using namespace minar;
 class I2STest {
 
 public:
-    I2STest(): i2s(YOTTA_CFG_HARDWARE_TEST_PINS_I2S_DPIN, YOTTA_CFG_HARDWARE_TEST_PINS_I2S_SCLK,
-		   YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL, YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX) {
-	for (uint32_t i = 0; i < sizeof(tx_buf); i++) {
-	    tx_buf[i] = i + TEST_BYTE_TX_BASE;
+    I2STest():
+    	dev_i2c(YOTTA_CFG_HARDWARE_TEST_PINS_I2C_SDA, YOTTA_CFG_HARDWARE_TEST_PINS_I2C_SCL),
+    	sta350(&dev_i2c, PA_10,
+    		YOTTA_CFG_HARDWARE_TEST_PINS_I2S_DPIN, YOTTA_CFG_HARDWARE_TEST_PINS_I2S_SCLK,
+			YOTTA_CFG_HARDWARE_TEST_PINS_I2S_WSEL, YOTTA_CFG_HARDWARE_TEST_PINS_I2S_FDPX,
+			YOTTA_CFG_HARDWARE_TEST_PINS_I2S_MCLK) {
+    for (uint32_t i = 0; i < sizeof(tx_buf); i++) {
+    	     tx_buf[i] = 0x0;
+   	}
+	for (uint32_t i = 0; i < sizeof(tx_buf); i +=73) {
+	     tx_buf[i] = i + TEST_BYTE_TX_BASE;
 	}
+	if(sta350.Init(0xFF, 44100)) { // betzw: start with master channel muted
+		printf("%s(%d): sta350bw init failed!\r\n", __func__, __LINE__);
+		exit(-1);
+	}
+
+	printf("\r\nTransfer test inited!\r\n");
     }
     
+    void unMute(void) {
+    	printf("Switching volume on now!\r\n");
+
+    	sta350.SetVolume(0x0, 50);
+    }
+
     void start() {
         printf("Starting transfer test\r\n");
 
-        printf("Res is %d\r\n", i2s.transfer()
+        printf("Res is %d\r\n", sta350.dev_i2s.transfer()
 	       .tx(tx_buf, XFER_SIZE)
 	       .callback(I2S::event_callback_t(this, &I2STest::transfer_complete_cb), I2S_EVENT_ALL)
 	       .circular(true)
 	       .apply());
+
+        Scheduler::postCallback(mbed::util::FunctionPointer0<void>(this, &I2STest::unMute).bind()).delay(milliseconds(1500));
     }
 
 private:
@@ -86,12 +111,21 @@ private:
     void transfer_complete_cb(Buffer tx_buffer, Buffer rx_buffer, int narg) {
         (void)tx_buffer;
         (void)rx_buffer;
+        (void)narg;
 
-        printf("Transfer: event is %d\r\n", narg);
+        uint8_t tmp;
+
+    	if(sta350.dev_i2c->i2c_read(&tmp, 0x38 /* devAddr */, 0x2D /* STA350BW_STATUS */, 1) != 0) {
+        	printf("\r\nERROR!\r\n");
+        }
+    	if(tmp != 0x7F) {
+    		printf("status=0x%x\r\n", tmp);
+    	}
     }
 
 private:
-	I2S i2s;
+	DevI2C dev_i2c;
+    STA350BW sta350;
 	uint8_t tx_buf[XFER_SIZE];
 	uint8_t rx_buf[XFER_SIZE];
 };
